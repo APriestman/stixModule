9ff1a33608e325bf5872a6f957e75f882a7d1feb
APriestman <apriestman@basistech.com>
2015-01-30 14:01:40 -0500

    Name change to prepare to be included in Autopsy.

--- src/org/sleuthkit/autopsy/modules/stix/STIXReportModuleConfigPanel.java
+++ src/org/sleuthkit/autopsy/modules/stix/STIXReportModuleConfigPanel.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sleuthkit.autopsy.stix;
+package org.sleuthkit.autopsy.modules.stix;
 
 import java.io.File;
 import javax.swing.JFileChooser;
